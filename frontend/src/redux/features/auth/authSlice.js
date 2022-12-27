7ed359036ef4a80ed190476da7ac596c3925dea6
Saiket Das <saiketdas02@gmail.com>
2022-12-27 23:21:46 +0600

    AuthSlice initial structure

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -1,4 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import auth from "../../../firebase/firebase.config";
 
 const initialState = {
   email: "",
@@ -8,10 +11,38 @@ const initialState = {
   error: "",
 };
 
+const createUser = createAsyncThunk(
+  "auth/createUser",
+  async ({ email, password }) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+
+    return data;
+  }
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.email = action.payload;
+      })
+
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.email = "";
+        state.isError = true;
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default authSlice.reducer;
